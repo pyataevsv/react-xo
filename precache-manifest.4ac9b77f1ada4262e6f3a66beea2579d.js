df7ae9ca7ae208c920769cff5313cb6c2baab696
pyataevsv <pyataevsv@gmail.com>
2020-05-21 18:38:01 +0300

    Updates

---
+++ precache-manifest.4ac9b77f1ada4262e6f3a66beea2579d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259bcaba91c0ab1b7463ca35da2c6e10",
+    "url": "/react-xo/index.html"
+  },
+  {
+    "revision": "ff2ff5819e76437cbf4b",
+    "url": "/react-xo/static/css/main.de3da112.chunk.css"
+  },
+  {
+    "revision": "f0558174720effbbedf9",
+    "url": "/react-xo/static/js/2.b4682d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-xo/static/js/2.b4682d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2ff5819e76437cbf4b",
+    "url": "/react-xo/static/js/main.ec7abf3b.chunk.js"
+  },
+  {
+    "revision": "9fc3a7aa513843e59665",
+    "url": "/react-xo/static/js/runtime-main.e93dc62d.js"
+  }
+]);
